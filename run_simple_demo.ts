0f9d0f60f0dd42f15bc3841b0bed357a9f5d2a78
Your Name <weolopez@gmail.com>
2025-04-27 19:35:26 -0400

    chore: Add remaining files for publishing

--- run_simple_demo.ts
+++
@@ -1,6 +0,0 @@
-// Simple runner script to execute the simple demo.
-
-import { main } from "./src/app/simple_demo.ts";
-
-// Run the main function from the demo
-main();
--- test/mod.ts
+++
@@ -1,4 +0,0 @@
-// Test files entry point
-console.log("Ghostty Graphics Tests");
-
-// TODO: Implement tests for UI library and application code
